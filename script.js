8874e34a52ea6b23f23d248e5e58fbca32db0a49
sinaf1997 <sinafmekonnen659@gmail.com>
2025-08-01 11:50:17 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//script 
+async function getData() {
+  const apiKey = 'AIzaSyDsIp4BEwbOzv2LXGmv2UXWEeuJhxSBEqY';
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: "Explain how AI works in a few words"
+              }
+            ]
+          }
+        ]
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getData();
+console.log("Try programiz.pro");
+
+ 
